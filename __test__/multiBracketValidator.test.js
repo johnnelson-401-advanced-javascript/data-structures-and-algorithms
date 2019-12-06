070ebaf45546307051b9485839a8af7c43f910c8
JOHN NELSON <JohnNelson4850@gmail.com>
2019-12-05 20:08:56 -0800

    true cases pass

---
+++ __test__/multiBracketValidator.test.js
@@ -0,0 +1,17 @@
+const bracketValidator = require('../challenges/multiBracketValidation/multi-bracket-validation');
+
+describe('Tests bracketValidator returns a boolean representing if the brackets are balanced', () => {
+  it('Should return True in these cases', () => {
+    let str = '{}';
+    expect(bracketValidator(str)).toBe(true);
+    let str2 = '{}(){}';
+    expect(bracketValidator(str2)).toBe(true);
+    let str3 = '()[[Extra Characters]]';
+    expect(bracketValidator(str3)).toBe(true);
+    let str4 = '(){}[[]]';
+    expect(bracketValidator(str4)).toBe(true);
+    let str5 = '{}{Code}[Fellows](())';
+    expect(bracketValidator(str5)).toBe(true);
+  });
+
+});
---
+++ challenges/multiBracketValidation/multi-bracket-validation.js
@@ -0,0 +1,25 @@
+function bracketValidator(str) {
+  let arr = [];
+
+  for(let i = 0; i < str.length; i++) {
+    if(str[i] === '(' || str[i] === '{' || str[i] === '['){
+      arr.push(str[i]);
+    }
+    if(str[i] === ')' && arr[arr.length - 1] === '(' 
+    || str[i] === '}' && arr[arr.length - 1] === '{' 
+    || str[i] === ']' && arr[arr.length - 1] === '[') {
+      arr.pop();
+    } 
+    else if(str[i] === ')' && arr[arr.length - 1] !== '(' 
+    || str[i] === '}' && arr[arr.length - 1] !== '{' 
+    || str[i] === ']' && arr[arr.length - 1] !== '[') {
+      return false;
+    }
+  }
+  if(arr.length === 0)
+    return true;
+  console.log(arr);
+  return false; 
+
+}
+module.exports = bracketValidator;
