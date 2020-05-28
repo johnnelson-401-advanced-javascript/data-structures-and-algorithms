b33921c7d6924b2afb4a816aaf1bda0cb73892c7
JOHN NELSON <JohnNelson4850@gmail.com>
2020-05-28 15:19:07 -0700

    spacing errors

--- challenges/arrayShift/array-shift.js
+++ challenges/arrayShift/array-shift.js
@@ -1,6 +1,6 @@
 function shiftArray(arr, element) {
-	for(let i = arr.length; i > 0; i--) {
-		arr[i] = arr[i-1];
-	}
-	arr[0] = element;
+  for(let i = arr.length; i > 0; i--) {
+    arr[i] = arr[i - 1];
+  }
+  arr[0] = element;
 }
